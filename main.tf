5eaf161a537471b8a170fffa31972f704104e0e2
Navin Kumar <168159694+NavinKumarbanu@users.noreply.github.com>
2025-08-04 14:21:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "google_storage_bucket" "navin-bucket2025" {
   force_destroy            = true
   public_access_prevention = "enforced"
 }
+
+resource "google_storage_bucket" "navin-bucketnew2025" {
+  name                     = "navin-bucket900"
+  project                  = "basic-cabinet-466904-a5"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
